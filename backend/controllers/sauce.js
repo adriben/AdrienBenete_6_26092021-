e8f7b11ad2391c71a0d6fd8bee56eafec93c4600
Adrien Benete <adrbenete@gmail.com>
2021-10-26 18:19:28 +0900

    ajout des routes pour creer une sauce et ajout du middleware multer pour sauvegarder les images sur le disque dur

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,13 +1,21 @@
 const Sauce = require('../models/Sauce');
 
-// exports.createSauce = (req, res, next) =>{
-
-// }
+exports.createSauce = (req, res, next) =>{
+   const sauceObject = JSON.parse(req.body.sauce);
+   delete sauceObject._id;
+   const sauce = new Sauce({
+       ...sauceObject,
+       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+   });
+   sauce.save()
+   .then(()=> res.status(201).json({ message: "sauce created"}))
+   .catch(err => res.status(400).json({ err }))
+}
 
 exports.displayAllSauces = (req, res, next) => {
     Sauce.find()
     .then(sauce => res.status(200).json(sauce))
     .catch(err => res.status(400).json({ message: err}))
-
-
 }
+
+
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express();
 const sauceCtrl = require('../controllers/sauce');
+const multer = require('../middleware/multer-config');
 
 router.get('/', sauceCtrl.displayAllSauces);
+router.post('/', multer, sauceCtrl.createSauce);
 
 module.exports = router;
